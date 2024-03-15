2df40e63c10ad3ad46acaf8037480cc61fc854f9
Aakash Patel <aakashpatel315@gmail.com>
2024-03-15 17:48:24 +0530

    google generative ai

--- frontend/src/app/signin/page.tsx
+++ frontend/src/app/signin/page.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { Button } from "@/components/ui/button";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 import axios from "axios";
 import { z } from "zod";
 import {
@@ -17,6 +18,79 @@ import {
 import { Input } from "@/components/ui/input";
 
 const SingUp = () => {
+
+
+
+
+
+    const genAI = new GoogleGenerativeAI("AIzaSyAC6CWSxzL9GgDdIoBrYd830ZhRb_eQT9w");
+
+    // ...
+    
+    async function run() {
+        // For text-only input, use the gemini-pro model
+        const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+      
+        const prompt = `Make me a course on mongodb. suggest multiple modules on the Course along with the estimated time required to complete the course. Start with the basics first. Return me the output in JSON in the following format.
+        Example output:
+        [
+            {
+                "title": "Python Basics",
+                "description": "Learn the basics of Python programming"
+                "time-required": "2 hours"
+            },
+            {
+                "title": "Python Advanced",
+                "description": "Learn advanced Python programming"
+                "time-required": "10 hours"
+            }
+            ...MORE MODULES...
+        ]`
+      
+
+        const result:any = await model.generateContent(prompt);
+        const response = await result.response;
+        const text = response.text();
+        const jsonWithoutBackticks = text.replace(/^```json|```$/g, '');
+
+        // Parse the JSON data
+        const parsedData = JSON.parse(jsonWithoutBackticks);
+        
+        // Store the array of objects in a variable
+        
+        
+        // Output the variable containing the array of objects
+        console.log(parsedData);
+
+
+
+
+        // console.log(text);
+      }
+      
+      run();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
